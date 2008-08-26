613b7b9372b1e988bc5cd3bfe98af1a66a2df090
Regis <xu.regis@gmail.com>
2008-08-26 10:16:09 +0800

    bind toggle-window-split to C-=

--- init.el
+++ init.el
@@ -89,6 +89,33 @@ If p is negative, move up, otherwise, move down."
 				(move-selected-lines 1)
 			      (move-line 1))))
 
+;; toggle split window vertical or horizontal
+;; from http://www.emacswiki.org/cgi-bin/emacs-en/ToggleWindowSplit
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
+(global-set-key (kbd "C-=") 'toggle-window-split)
 
 
 ;; set key for hs-minor-mode
