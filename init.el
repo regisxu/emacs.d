6be800e09ec46e1fb0bcd581621294a667707d43
Regis <xu.regis@gmail.com>
2008-12-30 19:21:16 +0800

    vc just support Git

--- init.el
+++ init.el
@@ -365,7 +365,7 @@ If p is negative, move up, otherwise, move down."
         try-expand-whole-kill
         ))
 
-(setq vc-handled-backends (quote (CVS SVN Git)))
+(setq vc-handled-backends (quote (Git)))
 
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
