3118376dead5ca249cf87685c5dc02e14d179ac8
Regis <xu.regis@gmail.com>
2010-01-08 15:58:23 +0800

    separate unix specific configuration

--- init.el
+++ init.el
@@ -31,13 +31,6 @@
                               (newline)
                               (indent-according-to-mode)))
 
-(add-hook 'server-switch-hook
-          (lambda ()
-            (if server-clients
-                (local-set-key (kbd "C-x C-c") 'server-edit))))
-
-(global-set-key (kbd "C-x C-c") 'delete-frame)
-
 ;; recursively find file
 ;; TODO select word at point as file name
 ;; TODO add history
@@ -194,16 +187,6 @@ If p is negative, move up, otherwise, move down."
             ;; (setq dired-x-hands-off-my-keys nil)
             ))
 
-(defun sudo-edit (&optional arg)
-  (interactive "p")
-  (if arg
-      (find-file (concat "/sudo:root@localhost:" (read-file-name "File: ")))
-    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
-
-(defun sudo-edit-current-file ()
-  (interactive)
-  (find-alternate-file (concat "/sudo:root@localhost:" (buffer-file-name (current-buffer)))))
-
 ;;-------------------------------------customize option--------------------------------
 
 
@@ -335,30 +318,6 @@ If p is negative, move up, otherwise, move down."
 ;;load htmlize which convert highlight code to html
 (require 'htmlize)
 
-(defun init-window-frame (&optional frame)
-  "configurations only for window system, such as color-theme, fonts"
-  (select-frame frame)
-  (if window-system
-      (progn
-        (set-default-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-21-*-*-*-m-0-iso10646-1")
-        ;; load color-theme
-        (require 'color-theme)
-        (setq color-theme-is-global nil)
-        (color-theme-gnome2)
-
-        (custom-set-faces
-         ;; custom-set-faces was added by Custom.
-         ;; If you edit it by hand, you could mess it up, so be careful.
-         ;; Your init file should contain only one such instance.
-         ;; If there is more than one, they won't work right.
-         '(diff-added ((t (:foreground "Green2"))))
-         '(diff-removed ((t (:foreground "IndianRed2"))))))
-    (progn
-      (transient-mark-mode t))))
-
-;; install color-theme only in window-system
-(add-hook 'after-make-frame-functions 'init-window-frame)
-
 ;; Convenient To select from kill ring
 (require 'browse-kill-ring)
 (browse-kill-ring-default-keybindings)
@@ -411,7 +370,8 @@ If p is negative, move up, otherwise, move down."
 (setq vc-handled-backends (quote (Git)))
 
 (if (string= system-type "windows-nt")
-    (load-file "~/.emacs.d/windows.el"))
+    (load-file "~/.emacs.d/windows.el")
+  (load-file "~/.emacs.d/unix.el"))
 
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
---
+++ unix.el
@@ -0,0 +1,43 @@
+
+;; configure for daemon
+(global-set-key (kbd "C-x C-c") 'delete-frame)
+(add-hook 'server-switch-hook
+          (lambda ()
+            (if server-clients
+                (local-set-key (kbd "C-x C-c") 'server-edit))))
+
+(defun init-window-frame (&optional frame)
+  "configurations only for window system, such as color-theme, fonts"
+  (select-frame frame)
+  (if window-system
+      (progn
+        (set-default-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-21-*-*-*-m-0-iso10646-1")
+        ;; load color-theme
+        (require 'color-theme)
+        (setq color-theme-is-global nil)
+        (color-theme-gnome2)
+
+        (custom-set-faces
+         ;; custom-set-faces was added by Custom.
+         ;; If you edit it by hand, you could mess it up, so be careful.
+         ;; Your init file should contain only one such instance.
+         ;; If there is more than one, they won't work right.
+         '(diff-added ((t (:foreground "Green2"))))
+         '(diff-removed ((t (:foreground "IndianRed2"))))))
+    (progn
+      (transient-mark-mode t))))
+
+;; install color-theme only in window-system
+(add-hook 'after-make-frame-functions 'init-window-frame)
+
+
+(defun sudo-edit (&optional arg)
+  (interactive "p")
+  (if arg
+      (find-file (concat "/sudo:root@localhost:" (read-file-name "File: ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+(defun sudo-edit-current-file ()
+  (interactive)
+  (find-alternate-file (concat "/sudo:root@localhost:" (buffer-file-name (current-buffer)))))
+
