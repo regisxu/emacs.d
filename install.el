2fa4d3bf6f5d65103c6a4d8972efe7551811901f
Regis <xu.regis@gmail.com>
2010-10-13 16:57:57 +0800

    Add 'not-confirm' argument to all update functions
    So that we could make the whole update process done without user interactive input.

--- install.el
+++ install.el
@@ -1,36 +1,39 @@
 (setq my-packages-from-emacswiki '("auto-install.el" "browse-kill-ring.el" "htmlize.el"))
 
 ;; This function could download and install packages from emacswiki
-(defun my-update-packages-from-emacswiki ()
+(defun my-update-packages-from-emacswiki (&optional not-confirm)
   "Download and install packages from emacswiki.
 The package list is read from variable 'my-packages-from-emacswiki'"
   (interactive)
   (require 'auto-install)
   (setq auto-install-directory "~/.emacs.d/site-lisp/")
+  (setq auto-install-save-confirm (not not-confirm))
   (dolist (package my-packages-from-emacswiki)
     (message "Update package %s" package)
     (auto-install-from-emacswiki package)))
 
 (setq my-packages-from-url '("http://www.emacswiki.org/emacs/download/dired+.el"))
 
-(defun my-update-packages-from-url ()
+(defun my-update-packages-from-url (&optional not-confirm)
   "Download and install packages from URL.
 The URL list is read from variable 'my-packages-from-url'"
   (interactive)
   (require 'auto-install)
   (setq auto-install-directory "~/.emacs.d/site-lisp/")
+  (setq auto-install-save-confirm (not not-confirm))
   (dolist (package my-packages-from-url)
+    (message "Update package from %s" package)
     (auto-install-from-url package)))
 
-(defun my-update-all-required-packages ()
+(defun my-update-all-required-packages (&optional not-confirm)
   "Downlaond and install all required packages"
   (interactive)
   (if (not (file-exists-p "~/.emacs.d/site-lisp/auto-install.el"))
       (progn
         (url-copy-file "http://www.emacswiki.org/emacs/download/auto-install.el" "~/.emacs.d/site-lisp/auto-install.el")
         (byte-compile-file "~/.emacs.d/site-lisp/auto-install.el")))
-  (my-update-packages-from-emacswiki)
-  (my-update-packages-from-url))
+  (my-update-packages-from-emacswiki (not not-confirm))
+  (my-update-packages-from-url (not not-confirm)))
 
 (if (not (file-exists-p "~/.emacs.d/site-lisp/auto-install.el"))
-    (my-update-all-required-packages))
+    (my-update-all-required-packages t))
