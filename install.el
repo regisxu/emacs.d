3b7c30799059f717e426567ff769225ac2a230b7
Regis <xu.regis@gmail.com>
2011-11-08 00:37:23 +0800

    Improve install.el

--- install.el
+++ install.el
@@ -1,20 +1,22 @@
 (setq my-packages-from-emacswiki '("browse-kill-ring.el" "htmlize.el" "dired+.el" "batch-mode.el"))
+(setq my-site-lisp-location "~/.emacs.d/site-lisp/")
 
-(defun my-download-packages-from-emacswiki ()
-  (dolist (file-name my-packages-from-emacswiki)
-    (when (not (file-exists-p (concat "~/.emacs.d/site-lisp/" file-name)))
-      (message "Download package %s" file-name)
-      (url-copy-file (concat "http://www.emacswiki.org/emacs/download/" file-name)
-                     (concat "~/.emacs.d/site-lisp/" file-name))
-      (byte-compile-file (concat "~/.emacs.d/site-lisp/" file-name)))))
+(defun my-install-package (url file)
+  (when (not (file-exists-p file))
+    (message "Download package %s" url)
+    (url-copy-file url file)
+    (byte-compile-file file)))
 
-(defun my-download-color-theme ()
-  (interactive)
-  "Download and install the latest color-theme."
-  (when (not (file-exists-p "~/.emacs.d/site-lisp/color-theme.el.gz"))
-    (url-copy-file "http://ftp.twaren.net/Unix/NonGNU/color-theme/color-theme.el.gz"
-                   "~/.emacs.d/site-lisp/color-theme.el.gz")
-    (byte-compile-file "~/.emacs.d/site-lisp/color-theme.el.gz")))
+;; install packages from emacswiki
+(mapcar (lambda (file-name)
+          (my-install-package (concat "http://www.emacswiki.org/emacs/download/" file-name)
+                              (concat my-site-lisp-location file-name)))
+        my-packages-from-emacswiki)
 
-(my-download-packages-from-emacswiki)
-(my-download-color-theme)
+;; install color-theme
+(my-install-package "http://ftp.twaren.net/Unix/NonGNU/color-theme/color-theme.el.gz"
+                    "~/.emacs.d/site-lisp/color-theme.el.gz")
+
+;; install smex
+(my-install-package "http://github.com/nonsequitur/smex/blob/master/smex.el?raw=true"
+                    "~/.emacs.d/site-lisp/smex.el")
