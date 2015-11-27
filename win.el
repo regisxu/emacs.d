934584a433766ecc039ead07f36a1e5033a0e2c7
Regis <ruizxu@ebay.com>
2015-11-27 14:45:09 +0800

    Refactor win.el

--- win.el
+++ win.el
@@ -26,8 +26,7 @@
        (list (cons "\\.[cC][mM][dD]$" 'batch-mode))
        auto-mode-alist))
 
-(autoload 'batch-mode "batch-mode"
-     "DOS and Windows BAT files" t)
+(use-package batch-mode)
 
 (setq tramp-default-method "plink")
 
@@ -57,59 +56,59 @@ Also returns nil if pid is nil."
       (desktop-save desktop-dirname)))
 (add-hook 'auto-save-hook 'my-desktop-save)
 
-;; Add muse to load-path
-(add-to-list 'load-path "~/.emacs.d/site-lisp/win/muse-3.20/lisp")
+;; ;; Add muse to load-path
+;; (add-to-list 'load-path "~/.emacs.d/site-lisp/win/muse-3.20/lisp")
 
-;; Load muse
-(require 'muse-mode)
-(require 'muse-html)
-(require 'muse-project)
-(require 'muse-colors)
-(require 'muse-wiki)
+;; ;; Load muse
+;; (require 'muse-mode)
+;; (require 'muse-html)
+;; (require 'muse-project)
+;; (require 'muse-colors)
+;; (require 'muse-wiki)
 
-(muse-index-as-string t t)
+;; (muse-index-as-string t t)
 
-(setq muse-project-alist
-      '(("Regis" ("~/wiki/muse"
-                  :default "Welcome"
-                  :force-publish ("Indexes"))
-         (:base "html" :path "~/wiki/publish"))))
+;; (setq muse-project-alist
+;;       '(("Regis" ("~/wiki/muse"
+;;                   :default "Welcome"
+;;                   :force-publish ("Indexes"))
+;;          (:base "html" :path "~/wiki/publish"))))
 
-(setq muse-html-meta-content-type "text/html; charset=gb18030")
-;; (setq muse-html-content-coding "gb18030")
-;; (setq muse-html-charset-default "gb18030")
-;; (setq muse-html-coding-default "gb18030")
-;; (setq muse-html-encoding-default "gb18030")
+;; (setq muse-html-meta-content-type "text/html; charset=gb18030")
+;; ;; (setq muse-html-content-coding "gb18030")
+;; ;; (setq muse-html-charset-default "gb18030")
+;; ;; (setq muse-html-coding-default "gb18030")
+;; ;; (setq muse-html-encoding-default "gb18030")
 
-(setq muse-html-footer "
-    <!-- Page published by Emacs Muse ends here -->
-    <br></br>
-    <div class=\"navfoot\">
-      <hr />
-      <table width=\"100%\" border=\"0\" summary=\"Footer navigation\">
-        <col width=\"33%\" /><col width=\"34%\" /><col width=\"33%\" />
-        <tr>
-          <td align=\"left\">
-          </td>
-          <td align=\"center\">
-            <span class=\"foothome\">
-            <lisp>
-                (concat
-                \"<a href=\\\"\"
-                    (muse-wiki-resolve-project-page)
-                    \"\\\">Home</a>\"
-                \" | <a href=\\\"\"
-                       (muse-wiki-resolve-project-page (muse-project) \"Indexes\")
-                       \"\\\">Indexes</a>\")
-            </lisp>
-            </span>
-          </td>
-          <td align=\"right\">
-          </td>
-        </tr>
-      </table>
-    </div>
-  </body>
-</html>")
-(setq muse-html-style-sheet
-      "<link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" href=\"common.css\" />")
+;; (setq muse-html-footer "
+;;     <!-- Page published by Emacs Muse ends here -->
+;;     <br></br>
+;;     <div class=\"navfoot\">
+;;       <hr />
+;;       <table width=\"100%\" border=\"0\" summary=\"Footer navigation\">
+;;         <col width=\"33%\" /><col width=\"34%\" /><col width=\"33%\" />
+;;         <tr>
+;;           <td align=\"left\">
+;;           </td>
+;;           <td align=\"center\">
+;;             <span class=\"foothome\">
+;;             <lisp>
+;;                 (concat
+;;                 \"<a href=\\\"\"
+;;                     (muse-wiki-resolve-project-page)
+;;                     \"\\\">Home</a>\"
+;;                 \" | <a href=\\\"\"
+;;                        (muse-wiki-resolve-project-page (muse-project) \"Indexes\")
+;;                        \"\\\">Indexes</a>\")
+;;             </lisp>
+;;             </span>
+;;           </td>
+;;           <td align=\"right\">
+;;           </td>
+;;         </tr>
+;;       </table>
+;;     </div>
+;;   </body>
+;; </html>")
+;; (setq muse-html-style-sheet
+;;       "<link rel=\"stylesheet\" type=\"text/css\" charset=\"utf-8\" href=\"common.css\" />")
