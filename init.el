fa210841ebc632bed4b94c389af0b61ccc31e2a2
Regis <ruizxu@ebay.com>
2016-06-29 14:31:27 +0800

    Initial package.

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 ;;-----------------------------start my environment----------------------------------
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (load-file "~/.emacs.d/install.el")
 
 (global-set-key [delete] 'delete-char)
