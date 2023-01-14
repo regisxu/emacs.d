8f4c7ec78f50186ffd80a969edefa59c18e72edf
Regis Xu <regis.xu@airwallex.com>
2023-01-14 14:05:28 +0800

    Fix desktop-mode on unix.

--- unix.el
+++ unix.el
@@ -55,28 +55,11 @@
 
 (require 'desktop)
 (setq desktop-dirname "~/.emacs.d")
-;; automatically overriding stale desktop lock
-(defun emacs-process-p (pid)
-  "If pid is the process ID of an emacs process, return t, else nil.
-Also returns nil if pid is nil."
-  (when pid
-    (let ((attributes (process-attributes pid)) (cmd))
-      (dolist (attr attributes)
-        (if (string= "comm" (car attr))
-            (setq cmd (cdr attr))))
-      (if (and cmd (or (string= "emacs" cmd) (string= "emacs.exe" cmd))) t))))
-
-(defadvice desktop-owner (after pry-from-cold-dead-hands activate)
-  "Don't allow dead emacsen to own the desktop file."
-  (when (not (emacs-process-p ad-return-value))
-    (setq ad-return-value nil)))
-
 (desktop-save-mode t)
 (setq desktop-restore-eager 50)
 
 (defun my-desktop-save ()
   (interactive)
-  ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
-  (if (eq (desktop-owner) (emacs-pid))
-      (desktop-save desktop-dirname)))
+  (desktop-save desktop-dirname))
+
 (add-hook 'auto-save-hook 'my-desktop-save)
